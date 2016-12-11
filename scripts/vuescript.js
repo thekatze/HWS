6cf8d3b401ceaf528e99ea279fcbea1b0ed385cb
Leo <leofel.katz@gmail.com>
2016-12-11 17:36:14 +0100

    fixed shit

--- scripts/vuescript.js
+++ scripts/vuescript.js
@@ -1,30 +1,25 @@
 $(function(){
 
-  const Dashboard = {template: "<p>Hello</p>" }
-  const Homeworks = {template: "<p>Hello</p>"}
-  const Classes = {template: "<p>Hello</p>"}
-  const Leaderboards = {template: "<p>Hello</p>"}
-  const Profile = {template: "<p>Hello</p>"}
-  const NotFound = {template: "<p>Hello</p>"}
+  const Dashboard = {template: "<p>Dashboard</p>" }
+  const Homeworks = {template: "<p>HW</p>"}
+  const Classes = {template: "<p>C</p>"}
+  const Leaderboards = {template: "<p>L</p>"}
+  const Profile = {template: "<p>P</p>"}
+  const NotFound = {template: "<p>NF</p>"}
 
-  const routes = {
-    '/': Dashboard,
-    '/homeworks': Homeworks,
-    '/classes': Classes,
-    '/leaderboards': Leaderboards,
-    '/profile': Profile
-  }
+  const routes = [
+    {path: '/', component: Dashboard},
+    {path: '/homeworks', component: Homeworks},
+    {path: '/classes', component: Classes},
+    {path: '/leaderboards', component: Leaderboards},
+    {path: '/profile', component: Profile}
+  ]
 
-  new Vue({
-    el: '#app',
-    data: {
-      currentRoute: window.location.pathname
-    },
-    computed: {
-      ViewComponent() {
-        return routes[this.currentRoute] || NotFound
-      }
-    },
-    render(h) {return h(this.ViewComponent)}
+  const router = new VueRouter({
+    routes
   })
+
+  const app = new Vue({
+    router
+  }).$mount('#app')
 });
