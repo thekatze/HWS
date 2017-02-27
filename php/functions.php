7080b4f92c021f81f8fe060470062faaac7384cb
Michi2806 <michi2806@gmail.com>
2017-02-27 14:07:01 +0100

    just some logout fixes

--- php/functions.php
+++ php/functions.php
@@ -3,10 +3,5 @@
     return new PDO('mysql:host=localhost;dbname=d8abase;charset=utf8', 'root', '');
   }
 
-  function logout_user() {
-    if (isset($_SESSION)) {
-      session_unset();
-      session_destroy();
-    }
-  }
+
 ?>
