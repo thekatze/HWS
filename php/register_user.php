c38ffb10ea05a8a0750fea028b3a624768ebe25a
Michi2806 <michi2806@gmail.com>
2017-05-08 12:44:09 +0200

    Regristraion wörks

--- php/register_user.php
+++ php/register_user.php
@@ -1,6 +1,7 @@
 <?php
 
   include 'functions.php';
+  include 'constants.php';
 
   $pdo = create_pdo();
 
@@ -8,21 +9,25 @@
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->beginTransaction();
 
+    $post_data = file_get_contents("php://input");
+    $post_username = json_decode($post_data)->{'u'};
+    $post_password = json_decode($post_data)->{'pw'};
+    $post_email = json_decode($post_data)->{'e'};
+
     $get_user_stmt = $pdo->prepare("call insert_user(:username_in, :email_in, :passwd_in, :token_in, @a)");
-    $get_user_stmt->bindParam(':username_in', $username);
-    $get_user_stmt->bindParam(':email_in', $email);
+    $get_user_stmt->bindParam(':username_in', $post_username);
+    $get_user_stmt->bindParam(':email_in', $post_email);
     $get_user_stmt->bindParam(':passwd_in', $passwd);
     $get_user_stmt->bindParam(':token_in', $token);
 
-    $username = $_GET['username'];
-    $email = $_GET['email'];
-    $passwd = password_hash($_GET['passwd'], PASSWORD_DEFAULT);
+    $passwd = password_hash($post_password, PASSWORD_DEFAULT);
     $token = md5(uniqid(rand()));
 
     $get_user_stmt->execute();
-    echo $pdo->query("select @a")->fetch()[0];
 
     $get_user_stmt->closeCursor();
+
+
 /*
     require_once('mailer/class.phpmailer.php');
       $mail = new PHPMailer();
@@ -43,9 +48,11 @@
       $mail->Send();
 */
     $pdo->commit();
+    $response = array('response' => SUCCESS);
     //YAY hat geklappt
   } catch (Exception $e) {
     $pdo->rollBack();
+    $response = array('response' => SQL_FAIL);
   }
-
+  echo json_encode($response);
 ?>
